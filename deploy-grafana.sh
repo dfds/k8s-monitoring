05645b120cee86eaa13b4a0a55957e250a81a94f
Henrik Stanley Mortensen <hemor@dfds.com>
2019-07-22 16:10:24 +0200

    Created new deployment shellscript

---
+++ deploy-grafana.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+set -e
+
+echo 'Checking for required environment variables...'
+if [[ -z $NAMESPACE || -z $SLACK_CHANNEL || -z $SLACK_URL || -z $ADMIN_PASSWORD ]]; then
+    echo 'required environment variables missing'
+    exit 1
+fi
+
+# Generate variables from script input
+KUBE_ROLE="$NAMESPACE-fullaccess"
+export TILLER_NAMESPACE=$NAMESPACE
+ROOT_URL="https://%(domain)s/$NAMESPACE"
+
+# Define function to render values.yaml template
+function render_template() {
+  eval "echo \"$(cat $1)\""
+}
+
+# Generate values.yaml file from template
+function generate_values_yaml {
+  echo "Creating values.yaml file"
+  render_template grafana/template-helm-values.yaml > values.yaml
+}
+
+generate_values_yaml
+
+echo "Creating tiller service account"
+kubectl create serviceaccount --namespace $NAMESPACE tiller
+
+echo "Creating tiller rolebinding for service account"
+kubectl create rolebinding tiller --role=$KUBE_ROLE --serviceaccount=$NAMESPACE:"tiller" -n $NAMESPACE
+
+echo "Initializing tiller into namespace"
+helm init --service-account tiller
+
+echo "Applying configmaps"
+kubectl -n $NAMESPACE apply -f grafana/configmaps/
+
+echo "Waiting for tiller to finish deploying"
+sleep 20
+
+echo "Creating secret 'grafana-password'"
+secret="grafana-password"
+kubectl --namespace $NAMESPACE create secret generic "$secret" --from-literal=admin-user=admin --from-literal=admin-password="$ADMIN_PASSWORD"
+
+echo "Deploying Grafana through Helm"
+helm --namespace $NAMESPACE install stable/grafana --name grafana -f values.yaml --set admin.existingSecret="$secret"
