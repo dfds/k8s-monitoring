ffc74c0eaac11e05461ff233415ac47011f22d9a
Henrik Stanley Mortensen <hemor@dfds.com>
2019-08-05 10:14:49 +0200

    Added JSON to Configmap convertion script

---
+++ grafana/Convert-JSONToConfigmap.ps1
@@ -0,0 +1,12 @@
+# Import all JSON files in folder
+$JSONFiles = Get-ChildItem -Name -File -Filter '*.json' -Path ".\dashboards"
+
+foreach ($File in $JSONFiles) {
+    $DASHBOARD_NAME = ($File.Substring(0,$File.Length-19) -replace ' ','-').ToLower()
+
+    # Generate configmap, indenting JSON 4 spaces
+    (Get-Content .\template-dashboard-cm.yaml) -replace "DASHBOARD_NAME", $DASHBOARD_NAME | Out-File .\configmaps\$($DASHBOARD_NAME)-cm.yaml
+    '    ' + (Get-Content .\dashboards\$($File) -Raw) -replace "`n", "`n    " | Out-File .\configmaps\$($DASHBOARD_NAME)-cm.yaml -Append
+
+    Get-ChildItem -Name -Path ".\configmaps"
+}
