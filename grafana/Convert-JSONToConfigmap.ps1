108251d6ff1930e5f928cc21eb2be01511f66033
Rasmus Rask <raras@dfds.com>
2019-08-08 11:59:08 +0200

    Fixed template path in Grafana convert script, ignoring CM yaml

--- grafana/Convert-JSONToConfigmap.ps1
+++ grafana/Convert-JSONToConfigmap.ps1
@@ -1,14 +1,15 @@
 # Import all JSON files in folder
 $JSONFiles = Get-ChildItem -Name -File -Filter '*.json' -Path ".\dashboards"
 
-Write-Output "Deploying the following ConfigMaps:"
+Write-Output "Converting the following ConfigMaps:"
 
 foreach ($File in $JSONFiles) {
-    $DASHBOARD_NAME = ($File.Substring(0,$File.Length-19) -replace ' ','-').ToLower()
+    $DASHBOARD_NAME = ($File.Substring(0, $File.Length - 19) -replace ' ', '-').ToLower()
 
     # Generate configmap, indenting JSON 4 spaces
-    (Get-Content .\template-dashboard-cm.yaml) -replace "DASHBOARD_NAME", $DASHBOARD_NAME | Out-File .\configmaps\$($DASHBOARD_NAME)-cm.yaml
+    (Get-Content .\templates\template-dashboard-cm.yaml) -replace "DASHBOARD_NAME", $DASHBOARD_NAME | Out-File .\configmaps\$($DASHBOARD_NAME)-cm.yaml
     '    ' + (Get-Content .\dashboards\$($File) -Raw) -replace "`n", "`n    " | Out-File .\configmaps\$($DASHBOARD_NAME)-cm.yaml -Append
 
-    Get-ChildItem -Name -Path ".\configmaps"
 }
+
+Get-ChildItem -Name -Path ".\configmaps"
