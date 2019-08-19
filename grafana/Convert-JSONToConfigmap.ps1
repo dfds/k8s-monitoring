053dc68306a7458b164c27c99566571629711132
Rasmus Rask <raras@dfds.com>
2019-08-19 15:21:47 +0200

    ConverJSONToConfigmap: Replace spaces with dashes in dash name

--- grafana/Convert-JSONToConfigmap.ps1
+++ grafana/Convert-JSONToConfigmap.ps1
@@ -4,7 +4,7 @@ $JSONFiles = Get-ChildItem -Name -File -Filter '*.json' -Path ".\dashboards"
 Write-Output "Converting the following ConfigMaps:"
 
 foreach ($File in $JSONFiles) {
-    $DASHBOARD_NAME = ($File -replace "-[0-9]{13}", "" -replace ".json", "").ToLower()
+    $DASHBOARD_NAME = ($File -replace "-[0-9]{13}", "" -replace " ", "-" -replace ".json", "").ToLower()
 
     # Generate configmap, indenting JSON 4 spaces
     (Get-Content .\templates\template-dashboard-cm.yaml) -replace "DASHBOARD_NAME", $DASHBOARD_NAME | Out-File .\configmaps\$($DASHBOARD_NAME)-cm.yaml
